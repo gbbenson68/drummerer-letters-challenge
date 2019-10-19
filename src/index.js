8fcbcb74669d8d473ba082e48f874a15dc7bb8d1
gbbenson68 <gbbenson68@gmail.com>
2019-10-19 09:50:53 -0400

    Import store, other objects
    Now import basic store, Provider and HashRouter components; refactored
    to make more readable.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { HashRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import store from './store'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const appJSX = (
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>
+)
+
+ReactDOM.render(appJSX, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.register()
