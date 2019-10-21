708a31d97387654f0da34334235b4090b631197a
gbbenson68 <gbbenson68@gmail.com>
2019-10-20 20:46:18 -0400

    Add abc -> letters translation

---
+++ src/lib/letters.js
@@ -0,0 +1,38 @@
+// These are Benny Greb's Letters, in ABC format
+export const notation4 = {
+  meter: 'M:4/4',
+  length: 'L:1/16',
+  letters: {
+    A: 'c4',
+    B: 'zc3',
+    C: 'z2c2',
+    D: 'z3c',
+    E: 'cc3',
+    F: 'zcc2',
+    G: 'z2cc',
+    H: 'c3c',
+    I: 'c2c2',
+    J: 'zc2c',
+    K: 'ccc2',
+    L: 'zccc',
+    M: 'c2cc',
+    N: 'cc2c',
+    O: 'cccc',
+    P: 'z4'
+  }
+}
+
+export const notation3 = {
+  meter: 'M:12/8',
+  length: 'L:1/8',
+  letters: {
+    Q: 'c3',
+    R: 'zc2',
+    S: 'z2c',
+    T: 'ccz',
+    U: 'zcc',
+    V: 'c2c',
+    W: 'ccc',
+    X: 'z3'
+  }
+}
